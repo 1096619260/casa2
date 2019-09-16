eebe042a50d6989bbb481eb473c7b587855c954d
1096619260 <sanabriaoscarj@gmailo.com>
2019-09-16 15:37:38 -0500

    hehe

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+ 
+value="<?php echo $va?>"
+ 
+</head>
+ 
+<body>
+
+<form name="f">
+  <p>Número 1: <input type="text" name="num1" value="0" onchange="cal()" onkeyup="cal()" /></p>
+  <p>Número 2: <input type="text" name="num2" value="0" onchange="cal()" onkeyup="cal()" /></p>
+  <p>Suma: <input type="number" name="sum" value="0" readonly="readonly" /></p>
+</form>
+
+</body>
+</html>
+ 
